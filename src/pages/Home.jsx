466b3bb4b3105a5db6294027c8e9610cd608f289
Vladislav899913 <Vladislav899913@gmail.com>
2024-04-16 13:58:21 +0300

    7 видео

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://661e415398427bbbef03f481.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
